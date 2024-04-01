f4ed4bac894fc7843b8e13ace8e9056258de470a
CarissaEngebose <132011642+CarissaEngebose@users.noreply.github.com>
2024-04-01 10:44:53 -0500

    Add files via upload

---
+++ game.dart
@@ -0,0 +1,12 @@
+
+class Game {
+  String team1; // string for team name 1
+  String team2; // string for team name 2
+  String workTeam; // string for work team
+  String time; // string for the time
+  int courtNum; // number for the court
+
+  /// constructor for team that requires team1, team2, workTeam, a time and a court number
+  Game({required this.team1, required this.team2, required this.workTeam, required this.time,
+   required this.courtNum,});
+}
