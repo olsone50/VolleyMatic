045aabbb328a9ab8907b9a5ed814fdcb4a2291de
Evan Olson <119567125+olsone50@users.noreply.github.com>
2024-04-03 10:42:55 -0500

    Update upcoming_tournaments_widget.dart

--- upcoming_tournaments_widget.dart
+++ upcoming_tournaments_widget.dart
@@ -12,7 +12,10 @@ class UpcomingTournaments extends StatefulWidget {
 }
 
 class UpcomingTournamentsWidget extends State<UpcomingTournaments> {
-@override
+
+  var tournaments = VolleymaticModel().tournaments;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -29,36 +32,63 @@ class UpcomingTournamentsWidget extends State<UpcomingTournaments> {
           )
         ],
       ),
-      body: ListView.builder(
-        itemCount: tournaments.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: const EdgeInsets.all(8.0),
-            color: Colors.red,
-            child: ListTile(
-              onTap: () {tapped(tournaments[index]);},
-              title: Text(
-                tournaments[index].name,
-                style: const TextStyle(color: Colors.white)),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Date: ${tournaments[index].date}',
-                    style: const TextStyle(color: Colors.white)),
-                  Text(
-                    'Location: ${tournaments[index].location}',
-                    style: const TextStyle(color: Colors.white)),
-                ],
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tournaments.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.red,
+                  child: ListTile(
+                    onTap: () { tapped(tournaments[index]); },
+                    title: Text(
+                      tournaments[index].name,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Date: ${tournaments[index].date}',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          'Location: ${tournaments[index].location}',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black,
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.add, color: Colors.white),
+                onPressed: () {
+                  //Handle Button Press
+                },
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
-  
- tapped(Tournament tournaments) {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => const MatchDetails()));         
+
+  tapped(Tournament tournaments) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const MatchDetails()));
   }
 }
