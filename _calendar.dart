724ada83e3fee828f09987331147fcb8081748e4
CarissaEngebose <engeboseca90@uwosh.edu>
2024-04-30 21:40:56 -0500

    update calendar to only display the closest tournament date as the day

--- _calendar.dart
+++ _calendar.dart
@@ -15,14 +15,29 @@ class Calendar extends StatefulWidget {
 }
 
 class CalendarWidget extends State<Calendar> {
-  DateTime? _selectedDate = DateTime.now(); // makes a variable for the current selected date
+  String? _closestTournamentDate;
+  DateTime? _selectedDate =
+      DateTime.now(); // makes a variable for the current selected date
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchClosestTournamentDate();
+  }
+
+  Future<void> _fetchClosestTournamentDate() async {
+    final closestDate = await widget.model.fetchClosestTournamentDate();
+    setState(() {
+      _closestTournamentDate = closestDate;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final DateTime now = DateTime.now();
     final DateTime firstDay = DateTime(now.year, now.month - 1, now.day);
     final DateTime lastDay = DateTime(now.year, now.month + 1, now.day);
-    final DateTime focusedDay = now; 
+    final DateTime focusedDay = now;
 
     return Column(
       children: [
@@ -36,12 +51,16 @@ class CalendarWidget extends State<Calendar> {
               weekendStyle: TextStyle(color: Colors.red),
             ),
             calendarStyle: CalendarStyle(
-              todayTextStyle: TextStyle(color: Colors.black), // makes the font color black for today's date so that it shows up
-              todayDecoration:  BoxDecoration(
-                border: Border.all(color: Colors.red), // outlines box for today's date
+              todayTextStyle: TextStyle(
+                  color: Colors
+                      .black), // makes the font color black for today's date so that it shows up
+              todayDecoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.red), // outlines box for today's date
                 shape: BoxShape.circle,
               ),
-              selectedDecoration: BoxDecoration( // makes the selected date have a red circle
+              selectedDecoration: BoxDecoration(
+                // makes the selected date have a red circle
                 color: Colors.red.withOpacity(0.8),
                 shape: BoxShape.circle,
               ),
@@ -64,38 +83,51 @@ class CalendarWidget extends State<Calendar> {
             },
           ),
         ),
-        if (_selectedDate != null) _buildTournamentList(), // displays the tournaments list for the date selected
+        if (_closestTournamentDate != null)
+          Container(
+            color: Colors.red, // Red background color
+            padding: EdgeInsets.all(8),
+            child: Text(
+              'Closest Tournament Date: $_closestTournamentDate',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        if (_selectedDate != null)
+          _buildTournamentList(), // displays the tournaments list for the date selected
       ],
     );
   }
 
   /// returns an {Expanded} widget that has a {ListView} of all the tournaments that are on a selected date
-  Expanded _buildTournamentList(){
+  Expanded _buildTournamentList() {
     return Expanded(
-      child: FutureBuilder (
-        future: widget.model.fetchTournamentsOnDate(DateFormat('MM-dd-yyyy').format(_selectedDate!)), // gets the tournaments from the database
-        builder:(context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator()); // display loading symbol if still loading
-              }
-                final tournaments = snapshot.data!;
-                return ListView.builder(
-                  itemCount: tournaments.length,
-                  itemBuilder: ((context, index) {
-                    final tournament = tournaments[index];
-                    return ListTile(
-                      leading: Container(height: 5, width: 5, color: Colors.red),
-                      title: Text(tournament['name']),
-                      subtitle: Text.rich(
-                        TextSpan(
-                          text: '${tournament['date']}\n${tournament['location']}',
-                        ),
-                      ),
-                    );
-                  }),
-                );
-            },
-        ),
+      child: FutureBuilder(
+        future: widget.model.fetchTournamentsOnDate(DateFormat('MM/dd/yyyy')
+            .format(_selectedDate!)), // gets the tournaments from the database
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+                child:
+                    CircularProgressIndicator()); // display loading symbol if still loading
+          }
+          final tournaments = snapshot.data!;
+          return ListView.builder(
+            itemCount: tournaments.length,
+            itemBuilder: ((context, index) {
+              final tournament = tournaments[index];
+              return ListTile(
+                leading: Container(height: 40, width: 10, color: Colors.red),
+                title: Text(tournament['name']),
+                subtitle: Text.rich(
+                  TextSpan(
+                    text: '${tournament['date']}\n${tournament['location']}',
+                  ),
+                ),
+              );
+            }),
+          );
+        },
+      ),
     );
   }
 }
