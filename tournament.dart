98816fa085de230380176958f339e08687b29b8d
Evan Olson <119567125+olsone50@users.noreply.github.com>
2024-05-16 20:33:01 -0500

    Update tournament.dart

--- tournament.dart
+++ tournament.dart
@@ -1,10 +1,13 @@
-
 class Tournament {
   String name; // stores the name of the tournament
-  String location ; // stores the location of the tournament
+  String location; // stores the location of the tournament
   DateTime date; // stores the date for the tournament
   int numCourts; // stores the number of courts for the tournament
 
   /// constructor for tournament that requires a name, location, date and number of courts to store
-  Tournament({required this.name, required this.location, required this.date, required this.numCourts});
+  Tournament(
+      {required this.name,
+      required this.location,
+      required this.date,
+      required this.numCourts});
 }
