c6ec4c7e914a40c4d0e8d1fecdcd45ac76afcc8c
Evan Olson <119567125+olsone50@users.noreply.github.com>
2024-05-16 20:32:52 -0500

    Update team.dart

--- team.dart
+++ team.dart
@@ -1,9 +1,12 @@
-
 class Team {
   String name; // stores the name of the team
   int rank; // stores the rank of the team
   String? rosterPath; // stores an image path of the roster for the team
 
   /// constructor for team that requires a name, rank, and roster image path to store
-  Team({required this.name, required this.rank, this.rosterPath,});
+  Team({
+    required this.name,
+    required this.rank,
+    this.rosterPath,
+  });
 }
